6be155735583d5be6f45ef71177128627a03f8f2
Billy Kaplan <billy1kaplan@gmail.com>
2016-12-15 17:53:15 -0600

    .

--- p1.ml
+++ p1.ml
@@ -1,20 +1,16 @@
 open Core.Std
 
-let rec last list =
-  match list with 
-  [] -> None
-  | [last_item] -> Some last_item
-  | hd::tl -> last tl
+let rec last = function
+  | [] -> None
+  | [x] -> Some x
+  | _::tl -> last tl
 ;;
-
-let a = last ["a";"b";"c";"d"];;
-let b = last [];;
 
 let f x = 
   match x with
-  Some c -> print_string (c ^ "\n")
-  | None -> print_string ("None" ^ "\n")
+  | Some c -> print_endline c 
+  | None -> print_endline "None"
 ;;
 
-f a;;
-f b;;
+f (last ["a"; "b"; "c"; "d"]);;
+f (last []);;
