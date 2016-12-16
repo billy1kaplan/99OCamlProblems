68e9191151a27ba4d92f944505c172417dcb8952
Billy Kaplan <billy1kaplan@gmail.com>
2016-12-15 21:34:38 -0600

    .

--- p1.ml
+++ p1.ml
@@ -4,13 +4,11 @@ let rec last = function
   | [] -> None
   | [x] -> Some x
   | _::tl -> last tl
-;;
 
 let f x = 
   match x with
   | Some c -> print_endline c 
   | None -> print_endline "None"
-;;
 
-f (last ["a"; "b"; "c"; "d"]);;
-f (last []);;
+  let () = f (last ["a"; "b"; "c"; "d"]);;
+  let () = f (last [])
--- p2.ml
+++ p2.ml
@@ -3,14 +3,12 @@ open Core.Std
 let rec last_two = function
   | [] | [_] -> None
   | [x;y] -> Some (x, y)
-  | _::tl -> last_two tl 
-;;
+  | _::tl -> last_two tl
 
 let f x = 
   match x with
   | Some (a, b) -> print_endline (a ^ ", " ^ b)
   | None -> print_endline "None"
-;;
 
-f (last_two ["a"; "b"; "c"; "d"]);;
-f (last_two []);;
+  let () = f (last_two ["a"; "b"; "c"; "d"])
+  let () = f (last_two [])
