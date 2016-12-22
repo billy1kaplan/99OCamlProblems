d938e5c3a2e3885fa511236f866025b614b645f4
Billy Kaplan <billy1kaplan@gmail.com>
2016-12-22 16:45:21 -0500

    .

--- p1.ml
+++ p1.ml
@@ -5,15 +5,9 @@ let rec last = function
   | [x] -> Some x
   | _::tl -> last tl
 
-<<<<<<< HEAD
-let f = function
-=======
-(* CR: use Option.value instead of match *)
-let f x = 
-  match x with
->>>>>>> 06d0075b0736f76bebabac4bf7917bcf860e40ad
-  | Some c -> print_endline c 
-  | None -> print_endline "None"
+(* XCR: use Option.value instead of match *)
+let f x = Option.value x ~default:"None" 
+          |> print_endline
 
 let () = f (last ["a"; "b"; "c"; "d"])
 let () = f (last [])
